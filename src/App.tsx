e6cb814cac0c859bde6b4db2eeabf62021f1ba95
Shubham Khandelwal <khandelwalshubham1992@gmail.com>
2025-01-04 23:03:08 +0100

    added papaparse, antd, xlsx and basic upload setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,59 @@
-function App() {
-  return <div>App Component !!</div>;
-}
+import React, { useEffect, useState } from "react";
+import { Button, Upload, UploadFile, UploadProps } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import Papa from "papaparse";
+
+const App = () => {
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const [uploading, setUploading] = useState(false);
+
+  useEffect(() => {
+    console.log(fileList);
+  }, [fileList]);
+
+  const props: UploadProps = {
+    onRemove: (file) => {
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      setFileList(newFileList);
+    },
+    beforeUpload: (file) => {
+      setFileList([...fileList, file]);
+
+      return false;
+    },
+    fileList,
+  };
+
+  const handleUpload = () => {
+    setUploading(true);
+    Papa.parse(fileList[0] as unknown as File, {
+      header: true,
+      complete: (results) => {
+        const data = results.data;
+        console.log(data);
+        setUploading(false);
+      },
+    });
+  };
+
+  return (
+    <div className="App">
+      <Upload {...props}>
+        <Button icon={<UploadOutlined />}>Upload</Button>
+      </Upload>
+      <Button
+        type="primary"
+        onClick={handleUpload}
+        disabled={fileList.length === 0}
+        loading={uploading}
+        style={{ marginTop: 16 }}
+      >
+        {uploading ? "Uploading" : "Start Upload"}
+      </Button>
+    </div>
+  );
+};
 
 export default App;
